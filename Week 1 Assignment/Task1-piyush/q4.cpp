270a19bd60645b7a1c27c565a51b458c6313a63c
piyushmishra06 <piyushmishra0620@gmail.com>
2025-03-29 22:37:22 +0530

    Updated q4.cpp

--- Week 1 Assignment/Task1-piyush/q4.cpp
+++ Week 1 Assignment/Task1-piyush/q4.cpp
@@ -13,7 +13,14 @@ int main(){
     while(revn2){
         octnumber=octnumber*10+(revn2%8);revn2/=8;
     }
-    cout<<"The binary equivalent of the number is "<<binnumber<<endl;
-    cout<<"The octal equivalent of the number is "<<octnumber<<endl;
+    int bin=0;int oct=0;
+    while(binnumber){
+        bin=bin*10+(binnumber%10);binnumber/=10;
+    }
+    while(octnumber){
+        oct=oct*10+(octnumber%10);octnumber/=10;
+    }
+    cout<<"The binary equivalent of the number is "<<bin<<endl;
+    cout<<"The octal equivalent of the number is "<<oct<<endl;
     return 0;
 }
