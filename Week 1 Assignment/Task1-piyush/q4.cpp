0ffbadb287984b92e5e8f743ec36bc82baaf9eee
piyushmishra06 <piyushmishra0620@gmail.com>
2025-03-29 22:52:14 +0530

    Updated q4.cpp

--- Week 1 Assignment/Task1-piyush/q4.cpp
+++ Week 1 Assignment/Task1-piyush/q4.cpp
@@ -2,16 +2,13 @@
 using namespace std;
 int main(){
     int n;cout<<"Enter a number belonging to the decimal number system:"<<endl;cin>>n;
-    int revn1=0;int revn2=0;
-    while(n){
-        revn1=revn1*10+(n%10);revn2=revn2*10+(n%10);n/=10;
-    }
+    int n1=n;
     int binnumber=0;int octnumber=0;
-    while(revn1){
-        binnumber=binnumber*10+(revn1%2);revn1/=2;
+    while(n){
+        binnumber=binnumber*10+(n%2);n/=2;
     }
-    while(revn2){
-        octnumber=octnumber*10+(revn2%8);revn2/=8;
+    while(n1){
+        octnumber=octnumber*10+(n1%8);n1/=8;
     }
     int bin=0;int oct=0;
     while(binnumber){
